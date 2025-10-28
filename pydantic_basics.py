8218f8d11c2e4acb261d0d6c546f2ce55763260d
pmokretsova <pmokretsova@tradingview.com>
2025-10-28 18:21:04 +0400

    Знакомство с Pydantic

---
+++ pydantic_basics.py
@@ -0,0 +1,143 @@
+"""
+{
+  "course": {
+    "id": "string",
+    "title": "string",
+    "maxScore": 0,
+    "minScore": 0,
+    "description": "string",
+    "previewFile": {
+      "id": "string",
+      "filename": "string",
+      "directory": "string",
+      "url": "https://example.com/"
+    },
+    "estimatedTime": "string",
+    "createdByUser": {
+      "id": "string",
+      "email": "user@example.com",
+      "lastName": "string",
+      "firstName": "string",
+      "middleName": "string"
+    }
+  }
+}
+"""
+
+import uuid
+from pydantic import BaseModel, Field, ConfigDict, computed_field, HttpUrl, EmailStr
+from pydantic.alias_generators import to_camel
+
+
+
+class FileSchema(BaseModel):
+    id: str
+    filename: str
+    directory: str
+    url: HttpUrl
+
+class UserSchema(BaseModel):
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name:str = Field(alias="middleName")
+
+    @computed_field
+    def username(self) -> str:
+        return f"{self. first_name}{self.last_name}"
+
+    def get_username(self) -> str:
+        return f"{self.first_name}{self.last_name}"
+
+class CourseSchema(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    title: str
+    max_score: int = Field(alias="maxScore")
+    min_score: int =Field(alias="minScore")
+    description: str
+    preview_file: FileSchema = Field(alias="previewFile")
+    estimated_time: str =Field(alias="estimatedTime")
+    created_by_user: UserSchema = Field(alias="createdByUser")
+
+
+course_default_model = CourseSchema(
+    id="course-id",
+    title="playwright",
+    maxScore=100,
+    minScore=10,
+    description="Playwright",
+    previewFile= FileSchema(
+        id="123",
+        filename="test1",
+        directory="courses",
+        url="http://localhost:8000/static/courses/test.png"
+    ),
+    estimatedTime="1 week",
+    created_by_user=UserSchema(
+        id="user-1",
+        email="alice@example.com",
+        firstName="Alice",    # благодаря populate_by_name можно и alias, и snake_case
+        lastName="Smith",
+        middleName=""
+    )
+)
+print("Course default data:", course_default_model)
+print(course_default_model.model_dump_json(by_alias=True))
+
+course_dict = {
+    "id": "course-id",
+    "title": "playwright",
+    "maxScore": 100,
+    "minScore": 10,
+    "description": "Playwright",
+    "estimatedTime": "1 week",
+    "previewFile": {
+        "id": "83af5dd1",
+        "filename": "test.png",
+        "directory": "courses",
+        "url": "http://localhost:8000/static/courses/test.png"
+    },
+    "createdByUser": {
+        "id": "user-1",
+        "email": "alice@example.com",
+        "firstName": "Alice",
+        "lastName": "Smith",
+        "middleName": ""
+    }
+}
+course_dict_model = CourseSchema(**course_dict)
+print("Course dict:", course_dict_model)
+
+course_json = """
+{
+  "id": "course-id",
+  "title": "playwright",
+  "maxScore": 100,
+  "minScore": 10,
+  "description": "Playwright",
+  "estimatedTime": "1 week",
+  "previewFile": {
+    "id": "83af5dd1",
+    "filename": "test.png",
+    "directory": "courses",
+    "url": "http://localhost:8000/static/courses/test.png"
+  },
+  "createdByUser": {
+    "id": "user-1",
+    "email": "alice@example.com",
+    "firstName": "Alice",
+    "lastName": "Smith",
+    "middleName": ""
+  }
+}
+"""
+course_json_model = CourseSchema.model_validate_json(course_json)
+print("Course json model:", course_json_model)
+print(course_json_model.model_dump_json(by_alias=True))
+
+
+
+
