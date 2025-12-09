93da3416c6578d3a1c08100e0a367c26dddad6b6
pmokretsova <pmokretsova@tradingview.com>
2025-12-09 19:43:10 +0400

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,13 @@
+def test_first_try():
+    print("Hello world!")
+
+
+def test_assert_positive_case():
+    assert (2 + 2 == 4)
+
+
+def test_assert_negative_case():
+    x=5
+    y=2
+    assert x == (y + y)
+
