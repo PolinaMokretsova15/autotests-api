a643714a54dbfc3f5577a5f104de7eb434d308fb
pmokretsova <pmokretsova@tradingview.com>
2026-02-10 18:42:41 +0400

    Маркировки Pytest

---
+++ tests/test_pytest_markers.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+# @pytest.mark.smoke
+# def test_smokre_case():
+#     assert 1 + 1 == 2
+#
+# @pytest.mark.regression
+# def test_regression_case():
+#     assert 2 * 2 == 4
+#
+# @pytest.mark.fast
+# def test_fast():
+#     ...
+#
+# @pytest.mark.slow
+# def test_slow():
+#     ...
+#
+# @pytest.mark.smoke
+# @pytest.mark.regression
+# @pytest.mark.critical
+# def test_critical_login():
+#     ...
+
+
