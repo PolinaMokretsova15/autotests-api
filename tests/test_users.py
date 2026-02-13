16a92fc0785180ddfe7f423836c818188e960721
pmokretsova <pmokretsova@tradingview.com>
2026-02-13 16:11:32 +0400

    Практика написания автотестов с использованием фикстур

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,12 +1,17 @@
-from clients.users.public_users_client import get_public_users_client, PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.public_users_client import PublicUsersClient
+from clients.users.users_schema import (
+    CreateUserRequestSchema,
+    CreateUserResponseSchema,
+    GetUserResponseSchema,
+)
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 import pytest
 
-from http import  HTTPStatus
+from http import HTTPStatus
 
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
+
 
 @pytest.mark.users
 @pytest.mark.regression
@@ -19,4 +24,15 @@ def test_create_user(public_users_client: PublicUsersClient):
     assert_status_code(response.status_code, HTTPStatus.OK)
     assert_create_user_response(request, response_data)
 
+    validate_json_schema(response.json(), response_data.model_json_schema())
+
+
+@pytest.mark.users
+@pytest.mark.regression
+def test_get_user_me(function_user, private_users_client):
+    """Проверяем эндпоинт GET /api/v1/users/me """
+    response = private_users_client.get_user_me_api()
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+    assert_get_user_response(response_data, function_user.response)
     validate_json_schema(response.json(), response_data.model_json_schema())
--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,9 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import (
+    UserSchema,
+    CreateUserRequestSchema,
+    CreateUserResponseSchema,
+    GetUserResponseSchema,
+)
 from tools.assertions.base import assert_equal
 
 
@@ -7,3 +12,14 @@ def assert_create_user_response(request:CreateUserRequestSchema, response:Create
     assert_equal(response.user.email, request.email, "last_name")
     assert_equal(response.user.email, request.email, "middle_name")
     assert_equal(response.user.email, request.email, "first_name")
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
+
+
+def assert_get_user_response(get_user_response: GetUserResponseSchema, create_user_response: CreateUserResponseSchema):
+    assert_user(get_user_response.user, create_user_response.user)
